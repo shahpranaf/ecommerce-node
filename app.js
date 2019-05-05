b5ba50b8b9c8bfdecb8d0484c40c59130f5d7f3b
shahpranaf <shahpranaf@gmail.com>
2019-05-06 01:10:01 +0530

    Commit with handlebars layout

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug'); // set view engine to pug
+// set engine as handlebars. its not built in node like pug.
+app.engine('hbs', expressHbs({ /* Provide directory of layouts */
+    layoutsDir : 'views/layouts/',
+    defaultLayout : 'main-layout',
+    extname: 'hbs'
+}));  // 1st arg is extension. set hbs or handlebars and create file with that extension. eg: 404.hbs
+app.set('view engine', 'hbs'); // set view engine to handlebars
 app.set('views', 'views'); // set view folder path. default is {project root}/view
 
 const adminData = require('./routes/admin');
