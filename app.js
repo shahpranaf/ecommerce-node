56365a2330e20f869a70446571a6c4cb8a634a3e
shahpranaf <shahpranaf@gmail.com>
2019-05-05 14:40:46 +0530

    Added pug layout

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug'); // set view engine to pug
+app.set('views', 'views'); // set view folder path. default is {project root}/view
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
