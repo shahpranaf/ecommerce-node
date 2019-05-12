678d4f5d8760cfe2a0a6535169a15b5f287fa44b
shahpranaf <shahpranaf@gmail.com>
2019-05-12 16:25:30 +0530

    Added Validation using express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
       api_key:
-        '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'
+        ''
     }
   })
 );
